0e403bede052ebc5ad6eb2a081c023988f0f77e5
Charlie Page <charliepage@charlies-mbp.attlocal.net>
2017-05-27 15:20:15 -0400

    init

---
+++ Redirection/Routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+$tmp_redirects = \App\Modules\Redirection\Models\Redirects::all();
+
+if (!empty($tmp_redirects)) {
+    $redirects = [];
+    foreach($tmp_redirects as $redirect) {
+        $redirects[$redirect->from_url] = $redirect->to_url;
+    }
+
+    $url_match = str_replace(URL::to('/'), '', Request::url());
+
+    if (isset($redirects[$url_match])) {
+        Route::get($url_match, [
+            'uses' => '\App\Modules\Redirection\Http\Controllers\RedirectsController@redirect'
+        ]);
+    }
+}
